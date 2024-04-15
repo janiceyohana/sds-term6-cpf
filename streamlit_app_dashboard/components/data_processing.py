dfbbde54450ee9a49568ae460360b2ffe2e41d60
Janice <janiceyohana@gmail.com>
2024-04-15 21:50:49 +0800

    delete unused file

--- streamlit_app_dashboard/components/data_processing.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-from statsmodels.tsa.arima.model import ARIMA
-import pandas as pd
-import matplotlib as plt
-
-def process_input_data(date_input, open_balance_input, new_cases_input, closed_cases_input):
-    # Placeholder for processing logic
-    st.success("Input data submitted successfully!")
